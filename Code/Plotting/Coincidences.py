dbd63790c779126bae787954381f02f042b63e4e
IsabelleJohn <isabelle.john@gmx.de>
2019-07-15 15:38:11 +0200

    fixed coincidence 3D

--- Code/Plotting/Coincidences.py
+++ Code/Plotting/Coincidences.py
@@ -27,6 +27,7 @@ def Coincidences_2D_plot(window):
     # Plot data
     fig = plt.figure()
     plt.suptitle('Coincident events (2D)\nData set(s): %s' % data_sets)
+
     # for 20 layers
     plt.subplot(1, 2, 1)
     plt.title('20 layers')
@@ -54,38 +55,90 @@ def Coincidences_2D_plot(window):
 # =============================================================================
 
 def Coincidences_3D_plot(df, window):
+    # Import data
+    df_20 = window.Clusters
+    df_16 = window.Clusters
+    # Perform initial filters
+    clusters_20 = filter_coincident_events(df_20, window)
+    clusters_16 = filter_coincident_events(df_16, window)
     # Declare max and min count
     min_count = 0
     max_count = np.inf
-    # Perform initial filters
-    df = filter_coincident_events(df, window)
     # Initiate 'voxel_id -> (x, y, z)'-mapping
-    MG24_ch_to_coord = get_MG24_to_XYZ_mapping()
+    MG24_ch_to_coord_20, MG24_ch_to_coord_16 = get_MG24_to_XYZ_mapping()
     # Calculate 3D histogram
-    H, edges = np.histogramdd(df[['wCh', 'gCh']].values,
+
+
+
+
+    # 20 layers
+    H_20, edges_20 = np.histogramdd(clusters_20[['wCh', 'gCh']].values,
                               bins=(80, 13),
                               range=((0, 80), (0, 13))
                               )
     # Insert results into an array
-    hist = [[], [], [], []]
-    loc = 0
-    labels = []
+    hist_20 = [[], [], [], []]
+    loc_20 = 0
+    labels_20 = []
     for wCh in range(0, 80):
         for gCh in range(0, 13):
-            over_min = H[wCh, gCh] > min_count
-            under_max = H[wCh, gCh] <= max_count
+            over_min = H_20[wCh, gCh] > min_count
+            under_max = H_20[wCh, gCh] <= max_count
             if over_min and under_max:
-                coord = MG24_ch_to_coord[gCh, wCh]
-                hist[0].append(coord['x'])
-                hist[1].append(coord['y'])
-                hist[2].append(coord['z'])
-                hist[3].append(H[wCh, gCh])
-                loc += 1
-                labels.append('Wire Channel: ' + str(wCh) + '<br>'
+                coord = MG24_ch_to_coord_20[gCh, wCh]
+                hist_20[0].append(coord['x'])
+                hist_20[1].append(coord['y'])
+                hist_20[2].append(coord['z'])
+                hist_20[3].append(H_20[wCh, gCh])
+                loc_20 += 1
+                labels_20.append('Wire Channel: ' + str(wCh) + '<br>'
                               + 'Grid Channel: ' + str(gCh) + '<br>'
-                              + 'Counts: ' + str(H[wCh, gCh])
+                              + 'Counts: ' + str(H_20[wCh, gCh])
                               )
+
+    # 16 layers
+    H_16, edges_16 = np.histogramdd(clusters_16[['wCh', 'gCh']].values,
+                              bins=(64, 13),
+                              range=((0, 64), (0, 13))
+                              )
+    # Insert results into an array
+    hist_16 = [[], [], [], []]
+    loc_16 = 0
+    labels_16 = []
+    for wCh in range(0, 64):
+        for gCh in range(0, 13):
+            over_min = H_16[wCh, gCh] > min_count
+            under_max = H_16[wCh, gCh] <= max_count
+            if over_min and under_max:
+                coord = MG24_ch_to_coord_16[gCh, wCh]
+                hist_16[0].append(coord['x'])
+                hist_16[1].append(coord['y'])
+                hist_16[2].append(coord['z'])
+                hist_16[3].append(H_16[wCh, gCh])
+                loc_16 += 1
+                labels_16.append('Wire Channel: ' + str(wCh) + '<br>'
+                              + 'Grid Channel: ' + str(gCh) + '<br>'
+                              + 'Counts: ' + str(H_16[wCh, gCh])
+                              )
+
     # Produce 3D histogram plot
+    labels = []
+    labels.extend(labels_20)
+    labels.extend(labels_16)
+
+    # Produce 3D histogram plot
+    hist = [[], [], [], []]
+    hist_x_offset = [i + 100 for i in hist_20[0]]
+    hist_z_offset = [i +  40 for i in hist_16[2]]
+    hist[0].extend(hist_x_offset)
+    hist[0].extend(hist_16[0])
+    hist[1].extend(hist_20[1])
+    hist[1].extend(hist_16[1])
+    hist[2].extend(hist_20[2])
+    hist[2].extend(hist_z_offset)
+    hist[3].extend(hist_20[3])
+    hist[3].extend(hist_16[3])
+
     MG_3D_trace = go.Scatter3d(x=hist[0],
                                y=hist[1],
                                z=hist[2],
@@ -131,11 +184,20 @@ def get_MG24_to_XYZ_mapping():
     LayerSpacing = 23.5
     GridSpacing = 23.5
     # Iterate over all channels and create mapping
-    MG24_ch_to_coord = np.empty((13, 80), dtype='object')
+    # 20 layers
+    MG24_ch_to_coord_20 = np.empty((13, 80), dtype='object')
     for gCh in np.arange(0, 13, 1):
         for wCh in np.arange(0, 80, 1):
             x = (wCh // 20) * LayerSpacing
             y = gCh * GridSpacing
             z = (wCh % 20) * WireSpacing
-            MG24_ch_to_coord[gCh, wCh] = {'x': x, 'y': y, 'z': z}
-    return MG24_ch_to_coord
+            MG24_ch_to_coord_20[gCh, wCh] = {'x': x, 'y': y, 'z': z}
+    MG24_ch_to_coord_16 = np.empty((13, 80), dtype='object')
+    # 16 layers
+    for gCh in np.arange(0, 13, 1):
+        for wCh in np.arange(0, 64, 1):
+            x = (wCh // 20) * LayerSpacing
+            y = gCh * GridSpacing
+            z = (wCh % 20) * WireSpacing
+            MG24_ch_to_coord_16[gCh, wCh] = {'x': x, 'y': y, 'z': z}
+    return MG24_ch_to_coord_20, MG24_ch_to_coord_16
